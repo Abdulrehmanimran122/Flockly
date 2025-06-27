cf2a0d5b3b4d5902edf39a70efa51b893c116676
Abdulrehman <techbers170@gmail.com>
2025-06-28 03:13:10 +0500

    Last Verification

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,9 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-
+app.get('/', (req, res) => {
+  res.send('Backend is working!');
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
